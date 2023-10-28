1a1c0beb61e8daa0f452d9865b3e6575f8f23938
Dmitry <149077334+Horclot@users.noreply.github.com>
2023-10-28 16:13:48 +0300

    add Registration

--- Page Navigation App/Page Navigation App/Window1.xaml.cs
+++ Page Navigation App/Page Navigation App/Window1.xaml.cs
@@ -1,21 +1,6 @@
-﻿using Microsoft.Data.Sqlite;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Markup;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
-using System.Security.Cryptography;
 
 
 namespace Page_Navigation_App
@@ -40,48 +25,55 @@ namespace Page_Navigation_App
             string email = emailReg.Text;
             string password = passReg.Password;
 
-            using (var context = new ApplicationDbContext())
+            if ((nameReg.Text == "") || (emailReg.Text == "") || (passReg.Password == "") || (!emailReg.Text.Contains("@")))
             {
-                // Проверка наличия имени пользователя в базе данных
-                if (context.Users.Any(u => u.Name == name))
-                {
-                    // Имя пользователя уже существует, обработайте ошибку или выведите сообщение
-                    MessageBox.Show("Данный пользователь уже зарегистрирован.");
-                    return; // Завершите метод
-                }
-
-                // Проверка наличия email в базе данных
-                if (context.Users.Any(u => u.Email == email))
-                {
-                    // Email уже существует, обработайте ошибку или выведите сообщение
-                    MessageBox.Show("Данный email уже зарегистрирован.");
-                    return; // Завершите метод
-                }
-
-                int id;
-                do
-                {
-                    // Генерируем случайный ID и проверяем его уникальность
-                    Random random = new Random();
-                    id = random.Next(1, 999999999);
-                }
-                while (context.Users.Any(u => u.Id == id));
-
-                var newUser = new User
-                {
-                    Id = id,
-                    Name = name,
-                    Email = email,
-                    Password = password
-                };
-
-                context.Users.Add(newUser);
-                context.SaveChanges();
+                MessageBox.Show("Ошибка заполнения данных","Ошибка",MessageBoxButton.OK,MessageBoxImage.Error);
             }
+            else
+            {
+                using (var context = new ApplicationDbContext())
+                {
 
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            this.Close();
+                    if (context.Users.Any(u => u.Name == name))
+                    {
+                        // Имя пользователя уже существует, обработайте ошибку или выведите сообщение
+                        MessageBox.Show("Данный пользователь уже зарегистрирован.");
+                        return;
+                    }
+
+
+                    if (context.Users.Any(u => u.Email == email))
+                    {
+                        // Email уже существует, обработайте ошибку или выведите сообщение
+                        MessageBox.Show("Данный email уже зарегистрирован.");
+                        return;
+                    }
+
+                    int id;
+                    do
+                    {
+                        // Генерируем случайный ID + уникальность
+                        Random random = new Random();
+                        id = random.Next(1, 999999999);
+                    }
+                    while (context.Users.Any(u => u.Id == id));
+
+                    var newUser = new User
+                    {
+                        Id = id,
+                        Name = name,
+                        Email = email,
+                        Password = password
+                    };
+
+                    context.Users.Add(newUser);
+                    context.SaveChanges();
+                }
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
         }
         
         private void Button_Click_2(object sender, RoutedEventArgs e)
